8e323d56635c1c2d59eaf023ece300732c9987fb
Alex Angelo <alex.angelo@dclick.com.br>
2026-06-16 14:00:29 -0300

    Adicionado Connection e Insert

--- simple_sql_builder/__init__.py
+++ simple_sql_builder/__init__.py
@@ -2,5 +2,9 @@ from simple_sql_builder.expression import E # Expression     Builder
 from simple_sql_builder.column     import A # AliasedColumn  Builder
 from simple_sql_builder.table      import T # Table + Column Builder
 
+# Connection
+from simple_sql_builder.connection import Connection
+
 # DMLs
+from simple_sql_builder.insert import Insert
 from simple_sql_builder.select import Select
