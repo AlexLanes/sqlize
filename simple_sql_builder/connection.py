a6762502ae66dc1bd3b79573b2b8d4cb520e9c4f
Alex Angelo <alex.angelo@dclick.com.br>
2026-06-18 10:18:37 -0300

    Correcao execute

--- simple_sql_builder/connection.py
+++ simple_sql_builder/connection.py
@@ -95,7 +95,7 @@ class Cursor:
     def execute (self, sql: str, params: SequenceAny | None = None, **kwargs) -> ResultSQL:
         self.cursor = (
             self.cursor.execute(sql, **kwargs)
-            if params is None else
+            if params else
             self.cursor.execute(sql, params, **kwargs)
         )
 
