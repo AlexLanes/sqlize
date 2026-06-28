06989c6978978a7402604fe5f3e57aa6863423cb
Alex Angelo <alex.angelo@dclick.com.br>
2026-06-28 12:27:02 -0300

    Adicionado odbc

--- simple_sql_builder/connections/__init__.py
+++ simple_sql_builder/connections/__init__.py
@@ -1,7 +1,8 @@
 """Package to automatically handle `Connections` of drivers and execute `Statements`
 - `Connection(conn)` Simplified wrapper to `DB API 2.0`
 - `ResultSQL` return `@dataclass` of `cursor.execute()` and `cursor.executemany()`
-### SQLite Connection `simple_sql_builder.connections.sqlite`
-### PostgreSQL Connection `simple_sql_builder.connections.postgresql` optional dependency `[postgresql]` needed"""
+#### SQLite Connection `simple_sql_builder.connections.sqlite`
+#### ODBC Connection `simple_sql_builder.connections.odbc` optional dependency `[odbc]` needed
+#### PostgreSQL Connection `simple_sql_builder.connections.postgresql` optional dependency `[postgresql]` needed"""
 
 from simple_sql_builder.connections.setup import Connection, ResultSQL
